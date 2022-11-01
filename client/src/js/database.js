74e048a8c7d45087955a28041bb5d5c1f7463a8b
Nick Stevens <90112060+stezzzy@users.noreply.github.com>
2022-11-01 17:44:25 -0400

    updated database and install files, updated webpack, installed dependencies

---
+++ client/src/js/database.js
@@ -0,0 +1,34 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({ id: 1, value: content});
+  const result = await request;
+  console.log('Data saved to database', result);
+};
+
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  return result.value;
+};
+
+
+initdb();
